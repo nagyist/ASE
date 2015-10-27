854fc924d1f3d0dc0d08a67d0755f8e28c88b221
suramyachukka <scw93@mail.umkc.edu>
2015-10-26 23:56:17 -0400

    lab8 submitted

---
+++ source/lab8/client/controllers.js
@@ -0,0 +1,351 @@
+angular.module('starter.controllers', [])
+
+
+.controller('LoginCtrl', function($scope, LoginService, DeleteService, UpdateService1, $ionicPopup, $state) {
+    $scope.data = {};
+ 
+    $scope.login = function(username,password) {
+        if(username == null)
+        {
+             var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please enter your username!'
+            })
+             }
+         if(password == null)
+        {
+             var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please enter your password!'
+            })
+             }
+            
+         LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+            $state.go('tab');
+        }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please check your credentials!'
+            });
+        });
+           
+    }
+    $scope.signup =function()
+    {
+        $state.go('register');
+    }
+     $scope.delete =function(username)
+    {
+         DeleteService.deleteUser($scope.data.username, $scope.data.password).success(function(data) {
+         var alertPopup = $ionicPopup.alert({
+                title: 'Deleted!',
+                template: 'Your account is deleted succesfully!'
+            });
+    }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Delete failed!',
+                template: 'Please check your credentials!'
+            });
+        });
+}
+      $scope.update =function(username)
+    {
+         UpdateService1.updateUser($scope.data.username, $scope.data.password).success(function(data) {
+         var alertPopup = $ionicPopup.alert({
+                title: 'Updated!',
+                template: 'Your details are updated succesfully!'
+            });
+    }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Update failed!',
+                template: 'Please check your details!'
+            });
+        });
+}
+}
+)
+.controller('RegisterCtrl', function($scope, RegisterService, $ionicPopup, $state) {
+     $scope.data = {};
+ 
+    $scope.register = function(email){
+        if($scope.data.email == null)
+        {
+            alert("please enter valid email id");
+        }else
+            if($scope.data.username == null)
+            {
+               alert("please enter all the fields"); 
+            }else
+                if($scope.data.password == null)
+            {
+               alert("please enter all the fields"); 
+            }else
+                 if($scope.data.firstname == null)
+            {
+               alert("please enter all the fields"); 
+            }else
+                if($scope.data.age == null)
+            {
+               alert("please enter all the fields"); 
+            }else
+                 if($scope.data.address == null)
+            {
+               alert("please enter all the fields"); 
+            }
+                
+        else
+        {
+      
+            RegisterService.RegisterUser($scope.data.firstname, $scope.data.lastname, $scope.data.address, $scope.data.age, $scope.data.email, $scope.data.username, $scope.data.password ).success(function(data) {
+           var alertPopup = $ionicPopup.alert({
+                title: 'Registration Success!',
+                template: 'Login to continue!'
+            });
+                $state.go('login');
+        }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Registration failed!',
+                template: 'Please check your details!'
+            });
+        });
+    }
+    }
+})
+.controller('SearchCtrl', function($scope, PatientService, $ionicPopup, $state) {
+   $scope.view = function (mySelect) {
+      
+       
+        $scope.data = {};
+       
+      
+  var a=mySelect;
+      
+       if( a != null)
+       {
+            $state.go('doctor');
+           return true;
+           
+          
+       }
+       else
+       {
+           
+            alert("Please select an option");
+           return false;
+       }
+}
+   $scope.showSelectValue = function(mySelect) {
+    alert(mySelect);
+}
+})
+.controller('ViewCtrl', function($scope, DoctorService, $ionicPopup, $state) {
+   $scope.d1 = function () {
+     
+  $state.go('doctor1');
+}
+   
+    $scope.d2 = function () {
+  $state.go('doctor1');
+}
+     $scope.d3 = function () {
+  $state.go('doctor1');
+}
+   
+})
+.controller('ScheduleCtrl', function($scope,Doctor1Service, $ionicPopup, $state) {
+   
+   $scope.appointment= function () {
+
+     if (window.confirm('Appointment Scheduled!'))
+{
+    $state.go('tab');
+}
+          
+          
+      
+}
+    $scope.View= function () {
+  
+       $state.go('camera');
+}
+})
+.controller('HomeCtrl', function($scope, Doctor1Service, $ionicPopup, $state) {
+   $scope.go= function () {
+       $state.go('login');
+   }
+})
+.controller('ConfirmCtrl', function($scope,Doctor1Service, $ionicPopup, $state) {
+   $scope.Session= function () {
+      
+       $state.go('session');
+   }
+})
+.controller('SettingsCtrl', function($scope,DeleteService, $ionicPopup, $state) {
+   
+   $scope.delete= function () {
+      
+       $state.go('delete');
+   }
+})
+.controller('DeleteCtrl', function($scope,DeleteService, $ionicPopup, $state) {
+    $scope.data = {};
+    
+   $scope.deleteaccount= function () {
+    
+       alert("Are you sure you want to delete your account? ");
+       DeleteService.deleteUser(localStorage.id).success(function(data) {
+         var alertPopup = $ionicPopup.alert({
+                title: 'Deleted!',
+                template: 'Your account is deleted succesfully!'
+            });
+    }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Delete failed!',
+                template: 'Please check your credentials!'
+            });
+        });
+      
+   }
+   
+})
+.controller('UpdateCtrl', function($scope, UpdateService1, UpdateService2, UpdateService3, $ionicPopup, $state) {
+    
+     $scope.data = {};
+    $scope.showstartCard1 = false;
+    $scope.showstartCard2 = false;
+      $scope.showstartCard3 = false;
+    $scope.hideCard1 = function() {
+        $scope.showstartCard1 = true;
+       // $scope.showsecondCard = true;
+    };
+     $scope.hideCard2 = function() {
+        $scope.showstartCard2= true;
+       // $scope.showsecondCard = true;
+    };
+     $scope.hideCard3 = function() {
+        $scope.showstartCard3= true;
+       // $scope.showsecondCard = true;
+    };
+    
+   $scope.change = function(oldusername,newusername) {
+       if($scope.data.oldusername == null )
+        {
+             var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please enter your oldusername!'
+            })
+             }
+         else if($scope.data.newusername== null)
+        {
+             var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please enter your new username!'
+            })
+             }
+       else
+       {
+            
+    
+         UpdateService1.updateUser($scope.data.oldusername,$scope.data.newusername).success(function(data) {
+         var alertPopup = $ionicPopup.alert({
+                title: 'Updated!',
+                template: 'Your details are updated succesfully!'
+            });
+    }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Update failed!',
+                template: 'Please check your details!'
+            });
+        });
+}}
+    $scope.change1 = function(oldpassword,newpassword) {
+      
+    
+         UpdateService2.updatePwd($scope.data.oldpassword,$scope.data.newpassword).success(function(data) {
+         var alertPopup = $ionicPopup.alert({
+                title: 'Updated!',
+                template: 'Your details are updated succesfully!'
+            });
+    }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Update failed!',
+                template: 'Please check your details!'
+            });
+        });
+}
+     $scope.change2 = function(oldemail,newemail) {
+      
+    
+         UpdateService3.updateEmail($scope.data.oldemail,$scope.data.newemail).success(function(data) {
+         var alertPopup = $ionicPopup.alert({
+                title: 'Updated!',
+                template: 'Your details are updated succesfully!'
+            });
+    }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Update failed!',
+                template: 'Please check your details!'
+            });
+        });
+}
+       
+       
+     
+   }
+)
+.controller("ExampleCtrl", function($scope, $cordovaCamera) {
+
+    $scope.takePicture = function() {
+        var options = { 
+            quality : 75, 
+            destinationType : Camera.DestinationType.DATA_URL, 
+            sourceType : Camera.PictureSourceType.CAMERA, 
+            allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 300,
+            targetHeight: 300,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false
+        };
+
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            $scope.imgURI = "data:image/jpeg;base64," + imageData;
+        }, function(err) {
+            // An error occured. Show a message to the user
+        });
+    }
+   
+
+} )
+.controller('SessionCtrl', function($scope, $cordovaCapture ,$state) {
+
+  $scope.captureAudio = function() {
+    var options = { limit: 3, duration: 10 };
+
+    $cordovaCapture.captureAudio(options).then(function(audioData) {
+      // Success! Audio data is here
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+  }
+  
+  $scope.Image = function() {
+     
+    $state.go('camera');
+     
+  }
+
+ 
+  $scope.captureVideo = function() {
+    var options = { limit: 3, duration: 15 };
+
+    $cordovaCapture.captureVideo(options).then(function(videoData) {
+      // Success! Video data is here
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+  }
+
+});
+;
+;
---
+++ source/lab8/client/services.js
@@ -0,0 +1,299 @@
+angular.module('starter.services', [])
+.service('LoginService', function($q, $http) {
+    return {
+  loginUser: function(name, pw) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+ 
+             $http({
+        method: 'GET',
+        url: 'http://lab8.mybluemix.net/rest/login/'+name,
+        contentType:"application/json"
+        
+    }).success(function(data){
+   //  alert(data[0]._id.$oid);
+            localStorage.setItem("firstname", data.firstname);
+          localStorage.setItem("lastname", data.lastname);
+          localStorage.setItem("username", data.username);
+          localStorage.setItem("password", data.password);
+          localStorage.setItem("id", data._id.$oid);
+      if (name == data.username && pw == data.password) {
+                deferred.resolve('Welcome ' + data.username + '!');
+            } else {
+                deferred.reject('Wrong credentials.');
+            }
+                 
+    })
+    promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+  }
+            
+        }})
+.service('DeleteService', function($q, $http) {
+    return {
+        
+         deleteUser:function(id) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+ 
+            
+         
+          $http({
+              method: 'DELETE' ,   
+              url: 'http://lab8.mybluemix.net/rest/delete/'+id,
+               contentType:"application/json"
+             }).success(function (data) { 
+              
+             deferred.resolve('Welcome ' + data.id + '!');
+             })
+               
+            
+    promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+
+        }
+        
+    }
+         })
+
+
+  .service('UpdateService1', function($q, $http) {
+    return {
+        
+         updateUser:function(oldname,newname) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+          $http({
+              method: 'POST' ,   
+        url: 'http://lab8.mybluemix.net/rest/update/'+oldname,
+		 data: JSON.stringify( { "$set" : { "username" : newname } } ),
+		 
+		  contentType: "application/json"
+             }).success(function (data) { 
+             })
+                deferred.resolve('Welcome !');
+            
+                 promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+    }
+    }
+         })
+
+.service('UpdateService2', function($q, $http) {
+    return {
+        
+         updatePwd:function(oldpwd,newpwd) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+ 
+             $http({
+        method: 'GET',
+        url: 'https://api.mongolab.com/api/1/databases/ase/collections/Patients?q={password:\''+oldpwd+'\'}&apiKey=ZE5gPXuMklJoxOhGZbFKK2tLg7SXx96I',
+        contentType:"application/json"
+        
+    }).success(function(data){
+  
+      if (oldpwd == data[0].password ) {
+         
+          $http({
+              method: 'PUT' ,   
+        url: 'https://api.mongolab.com/api/1/databases/ase/collections/Patients/'+data[0]._id.$oid+'?apiKey=ZE5gPXuMklJoxOhGZbFKK2tLg7SXx96I',
+		 data: JSON.stringify( { "$set" : { "password" : newpwd } } ),
+		 
+		  contentType: "application/json"
+             }).success(function (data) { 
+            ;
+             })
+                deferred.resolve('Welcome ' + data[0].username + '!');
+            } else {
+                deferred.reject('Wrong credentials.');
+            }
+                 
+    })
+    promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+
+        }
+        
+    }
+         })
+
+.service('UpdateService3', function($q, $http) {
+    return {
+        
+         updateEmail:function(oldemail,newemail) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+ 
+             $http({
+        method: 'GET',
+        url: 'https://api.mongolab.com/api/1/databases/ase/collections/Patients?q={email:\''+oldemail+'\'}&apiKey=ZE5gPXuMklJoxOhGZbFKK2tLg7SXx96I',
+        contentType:"application/json"
+        
+    }).success(function(data){
+  
+      if (oldemail == data[0].email ) {
+         
+          $http({
+              method: 'PUT' ,   
+        url: 'https://api.mongolab.com/api/1/databases/ase/collections/Patients/'+data[0]._id.$oid+'?apiKey=ZE5gPXuMklJoxOhGZbFKK2tLg7SXx96I',
+		 data: JSON.stringify( { "$set" : { "email" : newemail } } ),
+		 
+		  contentType: "application/json"
+             }).success(function (data) { 
+            ;
+             })
+                deferred.resolve('Welcome ' + data[0].username + '!');
+            } else {
+                deferred.reject('Wrong credentials.');
+            }
+                 
+    })
+    promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+
+        }
+        
+    }
+         })
+
+
+
+.service('RegisterService', function($q, $http) {
+    return {
+        RegisterUser: function(fname, lname, address, age, email, username, password) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+          $http({
+        method: 'POST',
+        url: 'http://lab8.mybluemix.net/rest/signup',
+        data: JSON.stringify({
+       firstname: fname,
+        lastname: lname,
+        address: address,
+        age: age,
+        email: email,
+        username: username,
+        password: password,
+    }),
+        contentType:"application/json"
+        
+    }).success(function(data){
+            
+            // alert(data);
+              deferred.resolve('Welcome!');
+            /* if ( data[0].username != null && data[0].password != null && data[0].lastname != null && data[0].firstname != null &&data[0].email != null ) {
+                deferred.resolve('Welcome ' + data[0].username + '!');
+            } else {
+                deferred.reject('please fill all the fields');
+            }
+              */ 
+    
+    })
+           promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+           
+        }
+    }
+})
+
+.service('PatientService', function($q) {
+    return {
+        Patient: function() {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+ 
+          
+            promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+        }
+    }
+})
+.service('DoctorService', function($q) {
+    return {
+        Doctor: function() {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+ 
+          
+            promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+        }
+    }
+})
+.service('Doctor1Service', function($q) {
+    return {
+        Doctor1: function() {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+ 
+          
+            promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+        }
+    }
+})
+;
